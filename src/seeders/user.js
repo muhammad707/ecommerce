acb25711211a694ee6d4e92261690386fa6bd0e9
Mukhammadjon Sirojiddinov <mukhammadjon.707s@gmail.com>
2021-10-21 20:13:42 +0500

    User seeder

---
+++ src/seeders/user.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const { UserModel } = require('../models/user.model');
+require('dotenv').config();
+
+const users = [
+  {
+    username: 'johndoe',
+    email: 'johndoe@gmail.com',
+    password: 'password123',
+    first_name: 'John',
+    last_name: 'Doe',
+    address: [{
+      street: 'Main Street',
+      flat_number: 72,
+      district: 'Avenue',
+      region: 'California'
+    }]
+  }
+];
+
+module.exports = async function() {
+  try {
+    const salt = await bcrypt.genSalt(process.env.SALT_ROUNDS);
+
+    await Promise.all(users.map(async user => user.password = await bcrypt.hash(user.password, salt)));
+
+    await UserModel.collection.drop();
+    await UserModel.insertMany(users);
+    console.log('Dummy Users inserted');
+  } catch (error) {
+    console.log('Error on dummy Users');
+    console.log(error);
+    process.exit(1);
+  }
+}
